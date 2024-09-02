8e3fc1da41e6eed38feafcefbdcffebea6d3afe8
harshdeep61034 <hdeep61034@gmail.com>
2024-09-02 12:53:43 +0530

    tweaks

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 export default function Page(){
     return (
         <main>
+            <div className="w-screen h-10 bg-pink-400"> connect wallet </div>
             <h1>Hi there</h1>
         </main>
     )
