4fb0294476a6c2ab15f65f52ca40d5689099e06d
Sudhanshu <sudhanshusaudiyal15@gmail.com>
2024-09-02 21:18:37 +0530

    param route

---
+++ app/superchat/[ythandle]/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { useParams } from "next/navigation"
+export default function(){
+    const {ythandle} = useParams();
+    return(
+        <div>
+            <div>
+
+            </div>
+            <div className="flex">
+                <div className="">
+
+                </div>
+                <div className="">
+
+                </div>
+
+            </div>
+       
+        </div>
+    )
+}
